3b8f51f611d38e3e9d565a73deb7c1c8d7a15f73
Lora Artois <lora.artois@student.ucll.be>
2024-12-08 07:21:12 +0100

    day 1 answer 2

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,47 @@
 use std::fs::File;
 use std::io::{self, BufRead};
+use std::ops::Add;
 use std::path::Path;
 
 fn main() {
+    similarity();
+}
+
+fn similarity() {
     let mut list0 = Vec::new();
     let mut list1 = Vec::new();
-    let mut result : i64 = 0;
+    let mut result: i64 = 0;
     // File hosts.txt must exist in the current path
     if let Ok(lines) = read_lines("./input.txt") {
         // Consumes the iterator, returns an (Optional) String
         for line in lines.flatten() {
-            let output: Vec<&str> = regex::Regex::new(" +").unwrap()
+            let output: Vec<&str> = regex::Regex::new(" +")
+                .unwrap()
+                .split(line.as_str())
+                .collect();
+            list0.push(output[0].parse::<i64>().unwrap());
+            list1.push(output[1].parse::<i64>().unwrap());
+        }
+    }
+
+    for i in 0..list0.len() {
+        let appearances: i64 = list1.iter().filter(|&x| *x == list0[i]).count().try_into().unwrap();
+        let similarity = list0[i] * appearances;
+        result = result.add(similarity);
+    }
+    println!("{}", result);
+}
+
+fn diff() {
+    let mut list0 = Vec::new();
+    let mut list1 = Vec::new();
+    let mut result: i64 = 0;
+    // File hosts.txt must exist in the current path
+    if let Ok(lines) = read_lines("./input.txt") {
+        // Consumes the iterator, returns an (Optional) String
+        for line in lines.flatten() {
+            let output: Vec<&str> = regex::Regex::new(" +")
+                .unwrap()
                 .split(line.as_str())
                 .collect();
             list0.push(output[0].parse::<i64>().unwrap());
